707158353f7a1d1c431607663aeb76137f05b80a
Elad Meidar <elad@eizesus.com>
2013-10-16 23:07:51 +0300

    [FIX]

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+BootstrapButtonsApp::Application.config.secret_key_base = 'ad540297a69746f96d2d07e9f81fb1fcc9ce9c359b77e546483dd2af724471f9d51da6f1b8620a7855dd79bc31ed7d659d42eed62ee323515e0f2fa8fd363744'
